24f13ad76946f7866f4edcc92f6a560b119fab4b
Tomas Petricek <tomas@tomasp.net>
2016-08-10 22:44:16 +0100

    Cleanup

--- src/app.fsx
+++ src/app.fsx
@@ -14,43 +14,77 @@ open Suave.Filters
 open Suave.Writers
 open Suave.Operators
 
+// ----------------------------------------------------------------------------
+// Various helper functions
+// ----------------------------------------------------------------------------
+
+// Find the 'web' directory with static files (like 'index.html'). This is 
+// a bit ugly, because it works differently locally and on Azure
 let root = 
   let asm =  
     if System.Reflection.Assembly.GetExecutingAssembly().IsDynamic then __SOURCE_DIRECTORY__
     else IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)
   IO.Path.GetFullPath(Path.Combine(asm, "..", "web"))
 
+/// Creates a URL for getting wether forecaset from OpenWeatherMap 
 let weatherUrl (place:string) = 
   "http://api.openweathermap.org/data/2.5/forecast/daily?q=" +
     HttpUtility.UrlEncode(place) +
     "&mode=json&units=metric&cnt=10&APPID=cb63a1cf33894de710a1e3a64f036a27"
 
+/// Creates a URL for getting stock prices form Yahooo
 let stocksUrl stock = 
   "http://ichart.finance.yahoo.com/table.csv?s=" + stock
-    
+
+/// Turn Unix timestamp into .NET DataTime object     
 let toDateTime (timestamp:int) =
   let start = DateTime(1970,1,1,0,0,0,DateTimeKind.Utc)
   start.AddSeconds(float timestamp).ToLocalTime()
 
+
+// ----------------------------------------------------------------------------
+// Type providers for accessing data
+// ----------------------------------------------------------------------------
+
 type Weather = JsonProvider<"http://api.openweathermap.org/data/2.5/forecast/daily?q=Prague&mode=json&units=metric&cnt=10&APPID=cb63a1cf33894de710a1e3a64f036a27">
 type BBC = XmlProvider<"http://feeds.bbci.co.uk/news/rss.xml">
 type Stocks = CsvProvider<"http://ichart.finance.yahoo.com/table.csv?s=MSFT">
+
+
+// ----------------------------------------------------------------------------
+// Web server and query answering
+// ----------------------------------------------------------------------------
 
 let answer (question:string) = async {
   let words = question.ToLower().Split(' ') |> List.ofSeq
   match words with
   | ["weather"; city] ->
       let! res = Weather.AsyncLoad(weatherUrl city)
-      let body = [ for r in res.List -> sprintf "<li><strong>%s</strong> &mdash; day %i&deg;C / night %i&deg;C</li>" ((toDateTime r.Dt).ToString("D")) (int r.Temp.Day) (int r.Temp.Night) ] |> String.concat ""
+      let items = 
+        [ for r in res.List -> 
+            sprintf "<li><strong>%s</strong> &mdash; day %i&deg;C / night %i&deg;C</li>" 
+              ((toDateTime r.Dt).ToString("D")) 
+              (int r.Temp.Day) (int r.Temp.Night) ] 
+      let body = items |> String.concat ""
       return Successful.OK ("<ul>" + body + "</ul>")      
+
   | ["news"] ->
       let! res = BBC.AsyncGetSample()
-      let body = [ for r in res.Channel.Items -> sprintf "<li><a href='%s'>%s</a></li>" r.Link r.Title ] |> Seq.take 10 |> String.concat ""
+      let items = 
+        [ for r in res.Channel.Items -> 
+            sprintf "<li><a href='%s'>%s</a></li>" r.Link r.Title ] 
+      let body = items |> Seq.take 10 |> String.concat ""
       return Successful.OK (sprintf "<ul>%s</ul>" body) 
+
   | ["stock"; stock] ->
       let! res = Stocks.AsyncLoad(stocksUrl stock)
-      let body = [ for r in res.Rows -> sprintf "<li><strong>%s</strong>: %f</li>" (r.Date.ToString("D")) r.Open ] |> Seq.take 10 |> String.concat ""
+      let items = 
+        [ for r in res.Rows -> 
+            sprintf "<li><strong>%s</strong>: %f</li>" 
+              (r.Date.ToString("D")) r.Open ] 
+      let body = items |> Seq.take 10 |> String.concat ""
       return Successful.OK (sprintf "<ul>%s</ul>" body) 
+
   | _ -> 
       return Successful.OK "Sorry Dave, I cannot do that." }
 
@@ -60,5 +94,4 @@ let app =
     path "/query" >=> fun ctx -> async {
         let! res = answer (HttpUtility.UrlDecode(ctx.request.rawQuery))
         return! res ctx }
-    Files.browse root
- ]
+    Files.browse root ]
